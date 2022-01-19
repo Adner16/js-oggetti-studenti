abc8551d76e2524c9ae1c388b44601f80f5eb8f8
Adner16 <giovanni.renda16@gmail.com>
2022-01-19 15:02:21 +0100

    added objects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log('js ok');
+
+
+// 1 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// 2 Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// 3 Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte).
+// 4 Ciclare su tutti gli studenti e stampare per ognuno di essi: nome e cognome.
+// 5 Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere all’array creato in precedenza, un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.
+// 6 Usiamo il DOM per stampare e chiedere le informazioni all'utente!
+
+const dispalyStudent = document.getElementById('list');
+
+//1 creare un oggetto // 2 stampare a schermo
+const student = { nome:'Giovanni', cognome:'renda', eta:'26'};
+for(let key in student){
+    console.log(student);
+    dispalyStudent.innerHTML = <li>${student}</li>
+}
+
+// 3 creare un arrey di oggetti
+
+// const classe = [
+//     giovanni = {nome: 'giovanni', cognome: 'renda', eta: '26'},
+//     antonio = {nome: 'antonio', cognome: 'pippo', eta: '20' },
+//     pietro = {nome: 'pietro', cognome: 'ciccio', eta: '21' },
+//     massimo = {nome: 'massimo', cognome: 'franco', eta: '22'},
+// ]
+// console.table(classe);
+
